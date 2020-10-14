13629d2b996320e958b5327e46099652962a19ac
Jared Barron <matthewjaredbarron@gmail.com>
2020-10-14 12:20:44 -0700

    Updating CPU calculation using Nodejs native os module

--- Challenge2/Client/src/components/App.jsx
+++ Challenge2/Client/src/components/App.jsx
@@ -19,10 +19,10 @@ const App = (props) => {
   const [initiated, initiate] = useState(false);
   const [info, setInfo] = useState([])
 
-  // useEffect(() => {
-  //   let data = ['5%', '10%', '3%', '30%', '80%', '30%', '3%']
-  //   setInfo(data)
-  // }, [])
+  useEffect(() => {
+    let data = ['5%', '10%', '3%', '30%', '80%', '30%', '3%']
+    setInfo(data)
+  }, [])
 
   const memory = () => {
     let data = {
@@ -63,40 +63,107 @@ const App = (props) => {
     console.log(data)
   }
 
-  // useEffect(() => {
-  //   let data1 = {
-  //     labels: ['60', '50', '40', '30', '20', '10', '0'],
-  //     datasets: [
-  //       {
-  //         label: `Memory Usage`,
-  //         fill: true,
-  //         lineTension: 0.5,
-  //         backgroundColor: 'rgba(46, 229, 157, .5)',
-  //         borderColor: 'rgba(46, 229, 157, 10)',
-  //         borderWidth: 2,
-  //         showLines: 'true',
-  //         data: info
-  //       }
-  //     ],
-  //   }
-  //   console.log(data)
-  //   // if (initiated === false) {
-  //     setData(data1);
-  //     console.log(data)
-  //   // }
-  //   // initiate(true);
-  // }, [info])
+  useEffect(() => {
+    let data1 = {
+      labels: ['60', '50', '40', '30', '20', '10', '0'],
+      datasets: [
+        {
+          label: `Memory Usage`,
+          fill: true,
+          lineTension: 0.5,
+          backgroundColor: 'rgba(46, 229, 157, .5)',
+          borderColor: 'rgba(46, 229, 157, 10)',
+          borderWidth: 2,
+          showLines: 'true',
+          data: info
+        }
+      ],
+    }
+    setData(data1);
+  }, [info])
 
-//   useEffect(() => {
-// //Create function to get CPU information
-// function cpuAverage() {
+  useEffect(() => {
+    //Create function to get CPU information
+    function cpuAverage() {
+
+      //Initialise sum of idle and time of cores and fetch CPU info
+      var totalIdle = 0, totalTick = 0;
+      var cpus = os.cpus();
+
+      //Loop through CPU cores
+      for (var i = 0, len = cpus.length; i < len; i++) {
+
+        //Select CPU core
+        var cpu = cpus[i];
+
+        //Total up the time in the cores tick
+        for (type in cpu.times) {
+          totalTick += cpu.times[type];
+        }
+
+        //Total up the idle time of the core
+        totalIdle += cpu.times.idle;
+      }
+
+      //Return the average Idle and Tick times
+      return { idle: totalIdle / cpus.length, total: totalTick / cpus.length };
+    }
+
+    // function to calculate average of array
+    const arrAvg = function (arr) {
+      if (arr && arr.length >= 1) {
+        const sumArr = arr.reduce((a, b) => a + b, 0)
+        return sumArr / arr.length;
+      }
+    };
+
+    // load average for the past 1000 milliseconds calculated every 100
+    function getCPULoadAVG(avgTime = 1000, delay = 100) {
+
+      return new Promise((resolve, reject) => {
+
+        const n = ~~(avgTime / delay);
+        if (n <= 1) {
+          reject('Error: interval to small');
+        }
+
+        let i = 0;
+        let samples = [];
+        const avg1 = cpuAverage();
+
+        let interval = setInterval(() => {
+          console.debug('CPU Interval: ', i);
+
+          if (i >= n) {
+            clearInterval(interval);
+            resolve(~~((arrAvg(samples) * 100)));
+          }
+
+          const avg2 = cpuAverage();
+          const totalDiff = avg2.total - avg1.total;
+          const idleDiff = avg2.idle - avg1.idle;
+
+          samples[i] = (1 - idleDiff / totalDiff);
+
+          i++;
+
+        }, delay);
+
+      });
+
+    }
+
+    getCPULoadAVG(1000, 100).then((avg) => {
+      console.log(avg);
+    });
+  }, setCPU(avg))
 
 
   return (
     <div className='background'>
       <div className='app'>
         <div className='body'>
-          <Header CPU={cpu} memory={memory} initiated={initiated} initiate={initiate}/>
+          <Header CPU={cpu} memory={memory} initiated={initiated} initiate={initiate} />
           <LineGraph data={data} />
           <Time />
         </div>
